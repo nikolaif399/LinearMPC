91cdbb3e0072c47a764386f2293de463f1b242c6
Roshan <roshannpradhan@gmail.com>
2020-12-13 00:21:22 -0500

    made some mods to drone matrices, added noise terms in sim, tuned

--- matlab/sim/drone/droneDynamics.m
+++ matlab/sim/drone/droneDynamics.m
@@ -10,14 +10,17 @@ roll=q(7);pitch=q(8);
 % u = roll, pitch, thrust  (command, angles in world frame);
 roll_command=u(1);
 pitch_command=u(2);
-thrust_command=u(3);
+thrust_command=u(3)*params.m;
 
 % Attitude controller here, update dpitch and droll
 dpitch = 100*(pitch_command - pitch);
 droll = 100*(roll_command - roll);
 
+omega = 1;
+acc_noise = [1; -1; 1]*max(sin(omega*t),0);
+
 R = rotz(0)*roty(rad2deg(pitch))*rotx(rad2deg(roll)); % roll and pitch in world frame so yaw irrelevant
-acc = [0;0;-params.g] + R*[0;0;thrust_command/params.m];
+acc = [0;0;-params.g] + R*[0;0;thrust_command/params.m] + acc_noise;
 
 dqdt = [dx;dy;dz;acc;droll;dpitch];
 
